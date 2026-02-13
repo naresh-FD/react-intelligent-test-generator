39d2c326a1d0ec464eff7b5c609bd76664615357
naresh-FD <116813860+Naresh-CES@users.noreply.github.com>
2026-02-13 15:54:14 +0530

    feat(testgen): remove coverage threshold settings from Jest config

--- examples/expense-manager/jest.config.js
+++ examples/expense-manager/jest.config.js
@@ -27,14 +27,6 @@ const config = {
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
 
 export default config;
